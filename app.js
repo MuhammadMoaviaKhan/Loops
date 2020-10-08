67deed6e335af2fc534cfe614bf850cb8f2300e1
MoaviaKhan <moaviakhan2001@gmail.com>
2020-10-08 16:32:29 +0500

    While Loop,Do While Loop and For Loop in JavaScript

---
+++ app.js
@@ -0,0 +1,46 @@
+// Topic 8 While loop
+var a = 1;
+
+while(a<=10){
+    document.write(a);
+    a++;
+    document.write("<br>");
+}
+document.write("<hr><br>")
+
+document.write("<br>")
+document.write("<h1>Table of 3 with While Loop</h1>")
+document.write("<br>")
+
+var b = 3;
+var c = 1;
+
+while(b<=30){
+    document.write("3 "+"x "+c+" = " +b);
+
+    b = b + 3;
+    c++
+    document.write("<br>");
+}
+
+// Topic 9 Do/While Loop
+
+document.write("<hr><br>")
+
+var a = 21;
+
+do{
+    document.write(a);
+    a++;
+    document.write("<br>");
+}while(a<=20);
+document.write("<hr><br>")
+
+// Topic 10 For Loop
+
+var d;
+
+for(d=0; d<10; d++){
+    document.write(d);
+    document.write("<br>");
+}
